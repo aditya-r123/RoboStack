7451c5665cd6309b5d6f06b07454847661a6d37c
Jishan Kharbanda <jishankharbanda@gmail.com>
2025-06-11 19:10:43 -0700

    fixed

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,6 +1,3 @@
-"""
-Register the custom environment so robosuite.make("MultiColorBlock", …) works.
-"""
 from robosuite.environments.base import register_env
 from .multi_color_block_env import MultiColorBlockEnv
 
